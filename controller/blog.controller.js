400aadd1026399d24976ad7cb7ae6156e5eead78
Rahul Deo Burman <burman@Rahuls-MacBook-Pro.local>
2024-08-12 12:09:25 +0530

    auth check removed

--- controller/blog.controller.js
+++ controller/blog.controller.js
@@ -34,14 +34,23 @@ function getblogsById(req, res) {
 }
 
 function getblogs(req, res) {
-    models.Blogs.findAll().then(result => {
+    models.Blogs.findAll()
+    .then(result => {
+        if (!result || result.length === 0) {
+            return res.status(404).json({
+                message: "No blogs found"
+            });
+        }
         res.status(200).json(result);
-    }).catch(error => {
+    })
+    .catch(error => {
+        console.error("Error fetching blogs:", error);
         res.status(500).json({
-            message: "something went wrong, please try again later!"
+            message: "Something went wrong, please try again later!"
         });
     });
 }
+
 
 module.exports = {
     addBlog: addBlog,
