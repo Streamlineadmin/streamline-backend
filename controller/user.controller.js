54b52cdcd20b638d164faeb050b5a5e67b34e928
Rahul Deo Burman <burman@Rahuls-MacBook-Pro.local>
2024-08-29 00:54:48 +0530

    add user api fixes

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -9,63 +9,31 @@ function addUser(req, res) {
                 message: "Email already exists !",
             });
         } else {
-            bcryptjs.genSalt(10, function (err, salt) {
-                bcryptjs.hash(req.body.password, salt, function (err, hashed) {
-                    console.log(hashed);
-                    const client = {
-                        name: req.body.name,
-                        email: req.body.email,
-                        contactNo: req.body.contactNo,
-                        username: req.body.username,
-                        password: hashed,
-                        role: req.body.role,
-                        companyName: req.body.companyName,
-                        companyId: req.body.companyId,
-                        ip_address: req.body.ip_address,
-                        status: 1
-                    }
+            const client = {
+                name: req.body.name,
+                email: req.body.email,
+                contactNo: req.body.contactNo,
+                username: req.body.username,
+                role: req.body.role,
+                companyName: req.body.companyName,
+                companyId: req.body.companyId,
+                ip_address: req.body.ip_address,
+                status: 1
+            }
 
-                    models.Users.create(client).then(result => {
-                        res.status(201).json({
-                            message: "User created successfully",
-                        });
-                    }).catch(error => {
-                        res.status(500).json({
-                            message: "Something went wrong!, Please try again later",
-                        });
-                    });
-                })
-            })
+            models.Users.create(client).then(result => {
+                res.status(201).json({
+                    message: "User created successfully",
+                });
+            }).catch(error => {
+                res.status(500).json({
+                    message: "Something went wrong!, Please try again later",
+                });
+            });
         }
     }).catch(error => {
         res.status(500).json({
             message: "Something went wrong!, Please try again later",
-        });
-    });
-
-
-    const user = {
-        companyId: req.body.companyId,
-        name: req.body.name,
-        email: req.body.email,
-        contactNo: req.body.contactNo,
-        username: req.body.username,
-        role: req.body.role,
-        companyName: req.body.companyName,
-        companyId: req.body.companyId,
-        ip_address: req.body.ip_address,
-        status: 1
-    }
-
-    models.Users.create(user).then(result => {
-        res.status(201).json({
-            message: "User added successfully",
-            post: result
-        });
-    }).catch(error => {
-        res.status(500).json({
-            message: "Something went wrong, please try again later!",
-            error: error
         });
     });
 }
