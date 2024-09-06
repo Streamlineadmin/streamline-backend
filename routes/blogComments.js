79b03cdeb3a01daa085f2c3564deddf422c8aff1
Rahul Deo Burman <burman@Rahuls-MacBook-Pro.local>
2024-09-06 23:37:02 +0530

    routes fixes

--- routes/blogComments.js
+++ routes/blogComments.js
@@ -5,6 +5,7 @@ const blogCommentsController = require('../controller/blogComments.conroller')
 const router = express.Router();
 router.post("/addBlogComments", blogCommentsController.addBlogComments);
 router.post("/getCommentstoApprove", blogCommentsController.getCommentstoApprove);
-router.post("/deleteBlogComment", blogCommentsController.deleteBlogComment); 
-router.post("/", blogCommentsController.getBlogComments);
+router.post("/deleteBlogComment", blogCommentsController.deleteBlogComment);
+router.post("/approveBlogComments", blogCommentsController.approveBlogComments);  
+router.post("/", blogCommentsController.getApprovedBlogComments);
 module.exports = router;
