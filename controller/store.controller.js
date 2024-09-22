74ebebf6fc642a77cadc26dfc6956bdb9a3fabaa
Rahul Deo Burman <burman@Rahuls-MacBook-Pro.local>
2024-09-22 21:45:18 +0530

    existing data checked for editing store

--- controller/store.controller.js
+++ controller/store.controller.js
@@ -53,7 +53,7 @@ function editStore(req, res) {
     ip_address: req.body.ip_address,
     addressLineOne: req.body.addressLineOne,
     addressLineTwo: req.body.addressLineTwo,
-    pincode: req.body.pinCode,
+    pincode: req.body.pincode,
     city: req.body.city,
     state: req.body.state,
     country: req.body.country,
@@ -61,17 +61,40 @@ function editStore(req, res) {
     status: req.body.status || 1,
   };
 
-  models.Store.update(updatedStoreData, { where: { id: storeId } })
-    .then((result) => {
-      if (result[0] > 0) {
-        res.status(200).json({
-          message: "Store updated successfully",
-          post: updatedStoreData,
+  // Check if store name already exists for another store in the same company
+  models.Store.findOne({
+    where: {
+      name: req.body.name,
+      companyId: req.body.companyId,
+      id: { [models.Sequelize.Op.ne]: storeId }, // Exclude current store
+    },
+  })
+    .then((storeResult) => {
+      if (storeResult) {
+        return res.status(409).json({
+          message: "Store name already exists!",
         });
       } else {
-        res.status(200).json({
-          message: "Something went wrong, please try again later!",
-        });
+        // Proceed to update the store if no conflicts are found
+        models.Store.update(updatedStoreData, { where: { id: storeId } })
+          .then((result) => {
+            if (result[0] > 0) {
+              res.status(200).json({
+                message: "Store updated successfully",
+                post: updatedStoreData,
+              });
+            } else {
+              res.status(404).json({
+                message: "Store not found",
+              });
+            }
+          })
+          .catch((error) => {
+            res.status(500).json({
+              message: "Something went wrong, please try again later!",
+              error: error,
+            });
+          });
       }
     })
     .catch((error) => {
@@ -81,6 +104,7 @@ function editStore(req, res) {
       });
     });
 }
+
 
 function deleteStore(req, res) {
   const storeId = req.body.storeId; // Assuming the store ID is passed as a URL parameter
