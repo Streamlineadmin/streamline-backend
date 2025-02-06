9c9c858aead24ed6f4e98d489253727ce85ddd30
sandeepanshu <sandeepks9199@gmail.com>
2025-02-06 23:05:14 +0530

    lock.json file add into git ignore

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
 
-const authenticationRoute = require('./routes/authentication');
-const fileRoute = require('./routes/file');
-const blogRoute = require('./routes/blogs');
-const teamRoute = require('./routes/teams');
-const userRoute = require('./routes/users');
-const storeRoute = require('./routes/stores');
-const addressRoute = require('./routes/address');
-const blogCommentsRoute = require('./routes/blogComments');
-const itemsRoute = require('./routes/items');
-const buyerSupplierRoute = require('./routes/buyerSupplier');
-const documentSeriesRoute = require('./routes/documentSeries');
-const accountDetailsRoute = require('./routes/accountDetails');
-const documentsRoute = require('./routes/documents');
-const notificationRoute = require('./routes/notifications');
-const botRoute = require('./routes/bot');
-const uomRoute = require('./routes/uom');
-const categoriesRoute = require('./routes/categories');
-const paymentTermsRoutes = require('./routes/paymentTerms');
-const logisticDetailsRoutes = require('./routes/logisticDetails');
-const transporterDetailsRoutes = require('./routes/transporterDetails');
-const termsConditionRoutes = require('./routes/termsCondition');
+const authenticationRoute = require("./routes/authentication");
+const fileRoute = require("./routes/file");
+const blogRoute = require("./routes/blogs");
+const teamRoute = require("./routes/teams");
+const userRoute = require("./routes/users");
+const storeRoute = require("./routes/stores");
+const addressRoute = require("./routes/address");
+const blogCommentsRoute = require("./routes/blogComments");
+const itemsRoute = require("./routes/items");
+const buyerSupplierRoute = require("./routes/buyerSupplier");
+const documentSeriesRoute = require("./routes/documentSeries");
+const accountDetailsRoute = require("./routes/accountDetails");
+const documentsRoute = require("./routes/documents");
+const notificationRoute = require("./routes/notifications");
+// const botRoute = require("./routes/bot");
+const uomRoute = require("./routes/uom");
+const categoriesRoute = require("./routes/categories");
+const paymentTermsRoutes = require("./routes/paymentTerms");
+const logisticDetailsRoutes = require("./routes/logisticDetails");
+const transporterDetailsRoutes = require("./routes/transporterDetails");
+const termsConditionRoutes = require("./routes/termsCondition");
 
 const app = express();
 
@@ -32,41 +32,41 @@ app.use(bodyParser.json());
 
 // Define the CORS options
 const corsOptions = {
-    origin: '*',
-    credentials: true, // Allow credentials (e.g., cookies, authorization headers)
+  origin: "*",
+  credentials: true, // Allow credentials (e.g., cookies, authorization headers)
 };
 
 // Use the cors middleware with your options
 app.use(cors(corsOptions));
 
 // Define route for the root URL
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-    //res.send("Welcome to EaseMargin APIs !");
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+  //res.send("Welcome to EaseMargin APIs !");
 });
 // Serve files from the 'uploads' folder
-app.use('/uploads', express.static('uploads'), fileRoute);
+app.use("/uploads", express.static("uploads"), fileRoute);
 
 // Use authentication routes for `/authentication` path
-app.use('/authentication', authenticationRoute);
-app.use('/blogs', blogRoute);
-app.use('/teams', teamRoute);
-app.use('/users', userRoute);
-app.use('/stores', storeRoute);
-app.use('/address', addressRoute);
-app.use('/blogComments', blogCommentsRoute);
-app.use('/items', itemsRoute);
-app.use('/buyerSupplier', buyerSupplierRoute);
-app.use('/documentSeries', documentSeriesRoute);
-app.use('/accountDetails', accountDetailsRoute);
-app.use('/documents', documentsRoute);
-app.use('/notification', notificationRoute);
-app.use('/bot', botRoute);
-app.use('/uom', uomRoute);
-app.use('/categories', categoriesRoute);
-app.use('/paymentTerms', paymentTermsRoutes);
-app.use('/logisticDetails', logisticDetailsRoutes);
-app.use('/transporterDetails', transporterDetailsRoutes);
-app.use('/termsCondition', termsConditionRoutes);
+app.use("/authentication", authenticationRoute);
+app.use("/blogs", blogRoute);
+app.use("/teams", teamRoute);
+app.use("/users", userRoute);
+app.use("/stores", storeRoute);
+app.use("/address", addressRoute);
+app.use("/blogComments", blogCommentsRoute);
+app.use("/items", itemsRoute);
+app.use("/buyerSupplier", buyerSupplierRoute);
+app.use("/documentSeries", documentSeriesRoute);
+app.use("/accountDetails", accountDetailsRoute);
+app.use("/documents", documentsRoute);
+app.use("/notification", notificationRoute);
+// app.use('/bot', botRoute);
+app.use("/uom", uomRoute);
+app.use("/categories", categoriesRoute);
+app.use("/paymentTerms", paymentTermsRoutes);
+app.use("/logisticDetails", logisticDetailsRoutes);
+app.use("/transporterDetails", transporterDetailsRoutes);
+app.use("/termsCondition", termsConditionRoutes);
 
 module.exports = app;
