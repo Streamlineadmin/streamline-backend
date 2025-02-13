4a1190ffa9b745c532c94cb2acb43573dd95b9c9
Rahul Deo Burman <burman@Rahuls-MacBook-Pro.local>
2025-02-13 21:55:24 +0530

    customer query model and migration added

---
+++ migrations/20250213162328-create-customer-query.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('CustomerQueries', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      query: {
+        type: Sequelize.STRING
+      },
+      ip_address: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('CustomerQueries');
+  }
+};
