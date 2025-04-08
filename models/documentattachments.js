58536d0ff2b48f139e8d7d82ffa6edbd53890633
sandeepanshu <sandeepks9199@gmail.com>
2025-04-08 15:39:39 +0530

    companyId aaded in attachments Table

--- models/documentattachments.js
+++ models/documentattachments.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   DocumentAttachments.init({
+    companyId: DataTypes.INTEGER,
     documentNumber: DataTypes.STRING,
     attachmentName: DataTypes.STRING
   }, {
